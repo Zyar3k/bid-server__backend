7fd8262e8953fd02c42866f82344dd5ba1c1894e
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-07 21:24:02 +0100

    Connect to DB

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6tpkm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 8888;
 
 const authRouter = require("./routes/auth");
@@ -22,6 +24,7 @@ app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
+    await connectDB(mongoUri);
     app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}`);
     });
