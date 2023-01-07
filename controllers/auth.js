241afd3ab3d74ec12b80c4bc1f47fa75067d5a5a
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-07 21:30:51 +0100

    Login - compare password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
   const newUser = await User.create({ ...req.body });
@@ -8,7 +9,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password, id } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Email and password are required");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid email or password");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid email or password");
+  }
+
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.OK)
+    .json({ user: { name: user.name, id: user._id }, token });
 };
 
 module.exports = {
