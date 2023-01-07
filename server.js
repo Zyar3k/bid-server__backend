764571f4b990380df7241e9110567872003796c4
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-07 21:19:50 +0100

    Add route/controllers/errors pattern

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8888;
 
+const authRouter = require("./routes/auth");
+const booksRouter = require("./routes/books");
+
+const notFoundMiddleware = require("./middleware/not-found.js");
+const errorHandlerMiddleware = require("./middleware/error-handler.js");
+
+app.use(express.json());
+
+app.use("/admin/auth", authRouter);
+app.use("/admin/books", booksRouter);
+
+app.use("/", booksRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const start = async () => {
   try {
     app.listen(PORT, () => {
